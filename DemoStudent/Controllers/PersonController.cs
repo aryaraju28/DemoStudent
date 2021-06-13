6caa38cbf24cbc1abd153d81cd36c8f0f8c59b73
aryaraju28 <arya28199@gmail.com>
2021-06-13 17:25:57 +0530

    Add project files.

---
+++ DemoStudent/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoStudent.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult StudentDetail()
+        {
+            //ViewBag.Name = "Arya";
+            //ViewBag.PresentTime = DateTime.Now.ToString();
+            ViewData["Name"] = "Arya";
+            ViewData["PresentTime"] = DateTime.Now.ToString();
+            ViewData["College"] = "SNGCE";
+
+
+
+            return View();
+        }
+    }
+}
